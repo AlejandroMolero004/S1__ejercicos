d40bc188a1a53a124267a4d7b0931cc49cc377d1
AlejandroMolero004 <amoleroo@alumnos.nebrija.es>
2023-09-18 19:39:36 +0200

    EJ 4 ARRAYLIST

---
+++ src/ej4.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ej4 {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        ArrayList<Double>array=new ArrayList<Double>();
+        double num;
+        int res=1;
+
+
+        while (res==1){
+            System.out.println("introduce un numero");
+            num=scan.nextDouble();
+            array.add(num);
+            System.out.println("quieres seguir? 1 si 0 no");
+            res=scan.nextInt();
+        }
+        for (double e:array){
+            System.out.print(e+" ");
+        }
+    }
+}
