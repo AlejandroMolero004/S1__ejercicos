5af8658a0cae8b6a8a39216a055ccafd5024e57a
AlejandroMolero004 <amoleroo@alumnos.nebrija.es>
2023-09-18 22:31:15 +0200

    Clase Mago

---
+++ src/ej8.java
@@ -0,0 +1,12 @@
+public class ej8 {
+    public static void main(String[] args){
+        Mago Gandalf=new Mago("Gandalf",12,100);
+        System.out.println("Hola soy: "+Gandalf.getNombre());
+        System.out.println("Tengo este nivel: "+Gandalf.getNivel());
+        System.out.println("Y tengo un nivel de mana de : "+Gandalf.getMana());
+
+
+
+    }
+}
+
